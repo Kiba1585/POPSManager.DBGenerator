ec7cf79be8340c05f7d9b77798bd354188efd779
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-05-13 19:33:55 +0200

    Actualizar ITranslator.cs

--- POPSManager.DBGenerator/Core/Interfaces/ITranslator.cs
+++ POPSManager.DBGenerator/Core/Interfaces/ITranslator.cs
@@ -2,5 +2,7 @@ namespace POPSManager.DBGenerator.Core.Interfaces;
 
 public interface ITranslator
 {
+    // Nuevo método para identificar el traductor en logs
+    string Name { get; }
     Task<string?> TranslateAsync(string text, string sourceLang, string targetLang);
 }
